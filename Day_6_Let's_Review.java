03738cd6afdb3142fea166d8687a2af55f4b3a8a
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-04-27 19:12:29 +0530

    Create Day_6_Let's_Review.java

---
+++ Day_6_Let's_Review.java
@@ -0,0 +1,94 @@
+/* Objective
+Today we will expand our knowledge of strings, combining it with what we have already learned about loops. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
+
+Note:  is considered to be an even index.
+
+Example
+
+
+Print abc def
+
+Input Format
+
+The first line contains an integer,  (the number of test cases).
+Each line  of the  subsequent lines contain a string, .
+
+Constraints
+
+Output Format
+
+For each String  (where ), print 's even-indexed characters, followed by a space, followed by 's odd-indexed characters.
+
+Sample Input
+
+2
+Hacker
+Rank
+Sample Output
+
+Hce akr
+Rn ak
+Explanation
+
+Test Case 0: 
+
+
+
+
+
+
+The even indices are , , and , and the odd indices are , , and . We then print a single line of  space-separated strings; the first string contains the ordered characters from 's even indices (), and the second string contains the ordered characters from 's odd indices ().
+
+Test Case 1: 
+
+
+
+
+The even indices are  and , and the odd indices are  and . We then print a single line of  space-separated strings; the first string contains the ordered characters from 's even indices (), and the second string contains the ordered characters from 's odd indices (). */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        in.nextLine();
+
+        for (int i = 0; i < N; i++) 
+        {
+            String string = in.nextLine();
+            char[] charArray = string.toCharArray();
+
+            for (int j = 0; j < charArray.length; j++) 
+            {
+                if (j % 2 == 0) 
+                {
+                    System.out.print(charArray[j]);
+                }
+            }
+
+            System.out.print(" ");
+
+            for (int j = 0; j < charArray.length; j++) 
+            {
+                if (j % 2 != 0) 
+                {
+                    System.out.print(charArray[j]);
+                }
+            }
+
+            System.out.println();
+        }
+
+        in.close();
+    }
+}
