cc22bb3fbac891c9103c9283256d04ab80f58d6d
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-04-28 16:57:17 +0530

    Create Day_10_Binary_Numbers.java

---
+++ Day_10_Binary_Numbers.java
@@ -0,0 +1,58 @@
+/* Objective
+Today, we're working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation. When working with different bases, it is common to show the base as a subscript.
+
+Example
+
+The binary representation of  is . In base , there are  and  consecutive ones in two groups. Print the maximum, .
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print a single base- integer that denotes the maximum number of consecutive 's in the binary representation of .
+
+Sample Input 1
+
+5
+Sample Output 1
+
+1
+Sample Input 2
+
+13
+Sample Output 2
+
+2
+Explanation
+
+Sample Case 1:
+The binary representation of  is , so the maximum number of consecutive 's is .
+
+Sample Case 2:
+The binary representation of  is , so the maximum number of consecutive 's is . */
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        // convert to binary and split into strings of consecutive ones
+        String[] groupings = Integer.toBinaryString(n).split("0");
+        int max = 0;
+        for(String s : groupings){
+            if(max < s.length()){
+                max = s.length();
+            }
+        }
+        System.out.println(max);
+    }
+}
